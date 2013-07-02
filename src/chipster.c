fcf5e68c79823b76b38b0a3c60cefebb6b0b6031
Aaron Weiss <aaronweiss74@gmail.com>
2013-07-02 12:53:33 -0400

    Updated CMakeLists.txt and fixed a stupid bug.

--- src/chipster.c
+++ src/chipster.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[]) {
 	check_user(access(argv[1], R_OK) != -1, "%s: could not find ROM: %s\n", argv[0], argv[1]);
 	if (!init() || !load(argv[1])) goto error;
 	for (;;) {
-		if (is_awaiting_keystroke) {
+		if (is_awaiting_keystroke()) {
 			update_keys();
 			for (uint8_t i = 0; i < 0x10; i++) if (get_key(i)) send_key(i);
 		} else {
