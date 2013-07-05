476e71ed23d0c30d595305deb3dac220f0e53da2
Aaron Weiss <aaronweiss74@gmail.com>
2013-07-05 00:41:05 -0400

    Added a simple test flag for brew formula.

--- src/chipster.c
+++ src/chipster.c
@@ -6,6 +6,7 @@
 
 int main(int argc, char *argv[]) {
 	check_user(argc >= 2, "%s: you must supply a ROM argument!\n", argv[0]);
+	if (!strcmp(argv[1], "--test")) return 0; // just a simple test to see if installed.
 	check_user(access(argv[1], R_OK) != -1, "%s: could not find ROM: %s\n", argv[0], argv[1]);
 	if (!init() || !load(argv[1])) goto error;
 	for (;;) {
